09226d0566dd8b009c57df4643c14164f3c2e76d
Sirotkina Aliona <leliaoff@ya.ru>
2018-07-07 15:48:32 +0400

    Signed-off-by: Sirotkina Aliona <leliaoff@ya.ru>

--- index.php
+++ index.php
@@ -12,36 +12,64 @@
     <link rel='stylesheet' href='css/svg.css' type='text/css' media='all'/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type='text/javascript' src='js/script.js'></script>
-    <link href="https://fonts.googleapis.com/css?family=Gaegu" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
 </head>
-<body>
+<body class="homepage">
     <div class="screen">
-        <h1 class="homepage">master code</h1>
-        <svg version="1.1" id="logo" title="Master Code - Делаем сложное простым" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-             width="256px" height="256.001px" viewBox="0 0 256 256.001" enable-background="new 0 0 256 256.001" xml:space="preserve">
-            <g>
-                <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="57.084" y1="87.5571" x2="41.5629" y2="123.8831">
-                    <stop offset="0"/><stop offset="1"/>
-                </linearGradient>
-                <polygon fill="url(#SVGID_1_)" points="48.066,87.318 66.303,87.318 48.367,123.732 34.711,123.732 	"/>
-                <linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="82.4116" y1="145.1782" x2="58.3105" y2="87.2052">
-                    <stop offset="0"/><stop offset="1"/>
-                </linearGradient>
-                <polygon fill="url(#SVGID_2_)" points="48.066,87.318 66.303,87.318 95.89,146.208 69.79,146.208 	"/>
-                <linearGradient id="SVGID_3_" gradientUnits="userSpaceOnUse" x1="129.0156" y1="50.1685" x2="85.0749" y2="145.3185">
-                    <stop offset="0"/><stop offset="1"/>
-                </linearGradient>
-                <polygon fill="url(#SVGID_3_)" points="104.889,50.902 152.463,50.902 95.89,146.208 69.79,146.208 	"/>
-                <linearGradient id="SVGID_4_" gradientUnits="userSpaceOnUse" x1="190.8789" y1="205.1533" x2="129.4258" y2="51.5206">
-                    <stop offset="0"/><stop offset="1"/>
-                </linearGradient>
-                <polygon fill="url(#SVGID_4_)" points="104.889,50.902 152.463,50.902 220.41,205.099 161.303,205.099 	"/>
-            </g>
-            <linearGradient id="SVGID_5_" gradientUnits="userSpaceOnUse" x1="246.3164" y1="178.2222" x2="9.6846" y2="77.778">
-                <stop offset="0"/><stop offset="1"/>
-            </linearGradient>
-            <circle fill="none" stroke="url(#SVGID_5_)" stroke-miterlimit="10" cx="128" cy="128" r="128"/>
-        </svg>
+        <div class="center">
+            <h1>master code</h1>
+            <h2 class="top-left"><a href="#">
+                <span><label>проектирование и</label></span>
+                <span><label>разработка веб-</label></span>
+                <span><label>приложений</label></span>
+                    <div class="line"></div>
+            </a></h2>
+            <h2 class="bottom-left"><a href="#">
+                <span><label>разработка</label></span>
+                <span><label>прикладных</label></span>
+                <span><label>приложений</label></span>
+                <span><label>под windows</label></span>
+                    <div class="line"></div>
+            </a></h2>
+            <h2 class="top-right"><a href="#">
+                <span><label>разработка и</label></span>
+                <span><label>поддержка</label></span>
+                <span><label>сайтов для бизнеса</label></span>
+                    <div class="line"></div>
+            </a></h2>
+            <h2 class="bottom-right"><a href="#">
+                <span><label>настройка и</label></span>
+                <span><label>сопровождение</label></span>
+                <span><label>linux-, windows-</label></span>
+                <span><label>серверов</label></span>
+                    <div class="line"></div>
+            </a></h2>
+            <svg version="1.1" id="logo" title="Master Code - Делаем сложное простым" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                 width="256px" height="256.001px" viewBox="0 0 256 256.001" enable-background="new 0 0 256 256.001" xml:space="preserve">
+                <g>
+                    <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="57.084" y1="87.5571" x2="41.5629" y2="123.8831">
+                        <stop offset="0"/><stop offset="1"/>
+                    </linearGradient>
+                    <polygon fill="url(#SVGID_1_)" points="48.066,87.318 66.303,87.318 48.367,123.732 34.711,123.732 	"/>
+                    <linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="82.4116" y1="145.1782" x2="58.3105" y2="87.2052">
+                        <stop offset="0"/><stop offset="1"/>
+                    </linearGradient>
+                    <polygon fill="url(#SVGID_2_)" points="48.066,87.318 66.303,87.318 95.89,146.208 69.79,146.208 	"/>
+                    <linearGradient id="SVGID_3_" gradientUnits="userSpaceOnUse" x1="129.0156" y1="50.1685" x2="85.0749" y2="145.3185">
+                        <stop offset="0"/><stop offset="1"/>
+                    </linearGradient>
+                    <polygon fill="url(#SVGID_3_)" points="104.889,50.902 152.463,50.902 95.89,146.208 69.79,146.208 	"/>
+                    <linearGradient id="SVGID_4_" gradientUnits="userSpaceOnUse" x1="190.8789" y1="205.1533" x2="129.4258" y2="51.5206">
+                        <stop offset="0"/><stop offset="1"/>
+                    </linearGradient>
+                    <polygon fill="url(#SVGID_4_)" points="104.889,50.902 152.463,50.902 220.41,205.099 161.303,205.099 	"/>
+                </g>
+                    <linearGradient id="SVGID_5_" gradientUnits="userSpaceOnUse" x1="246.3164" y1="178.2222" x2="9.6846" y2="77.778">
+                        <stop offset="0"/><stop offset="1"/>
+                    </linearGradient>
+                    <circle fill="none" stroke="url(#SVGID_5_)" stroke-miterlimit="10" cx="128" cy="128" r="128"/>
+            </svg>
+        </div>
     </div>
 </body>
 </html>
